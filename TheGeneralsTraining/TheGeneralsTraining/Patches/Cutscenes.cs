cb4c70946f18b3aad8a3b9e86264b9354ec46c5e
Gorzon38 <64686176+Gorzontrok@users.noreply.github.com>
2024-01-03 10:53:52 +0100

    Removed unused using

--- TheGeneralsTraining/TheGeneralsTraining/Patches/Cutscenes.cs
+++ TheGeneralsTraining/TheGeneralsTraining/Patches/Cutscenes.cs
@@ -1,9 +1,5 @@
 using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using TFBGames.Systems;
 using UnityEngine;
 
 namespace TheGeneralsTraining.Patches.Cutscenes
