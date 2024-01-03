4cda3a470eeb0d3b22ae03bc007e6a2c47bee7b3
Gorzon38 <64686176+Gorzontrok@users.noreply.github.com>
2024-01-03 10:52:21 +0100

    Removed Wardrobes from code

--- TheGeneralsTraining/TheGeneralsTraining/Dresser.cs
+++ TheGeneralsTraining/TheGeneralsTraining/Dresser.cs
@@ -8,40 +8,9 @@ namespace TheGeneralsTraining
 {
     public static class Dresser
     {
-        public static readonly Attire rescueBro = new Attire("TGT_RescueBro", ResourcesController.AssetsFolder)
-        {
-            wearer = nameof(RescueBro),
-            clothes = new Dictionary<string, string>()
-            {
-                { "sprite", "RescueBroFlexier.png" }
-            }
-        };
-        public static readonly Attire desperabro = new Attire("TGT_Desperabro", ResourcesController.AssetsFolder)
-        {
-            wearer = nameof(Desperabro),
-            clothes = new Dictionary<string, string>()
-            {
-                { "player.hud.avatar", "avatar_desperabro.png" }
-            }
-        };
-
-        public static readonly Attire broffy = new Attire("TGT_Broffy", ResourcesController.AssetsFolder)
-		{
-			wearer = "Broffy",
-			clothes = new Dictionary<string, string>
-			{
-				{
-					"sprite",
-					"broffy_anim.png"
-				}
-			}
-		};
-
         public static void Intitialize()
         {
-            AddToWardrobe(rescueBro);
-            AddToWardrobe(desperabro);
-			AddToWardrobe(broffy);
+            StorageRoom.AddSubscriber(ResourcesController.AssetsFolder);
         }
 
         public static void AddToWardrobe(Attire attire)
